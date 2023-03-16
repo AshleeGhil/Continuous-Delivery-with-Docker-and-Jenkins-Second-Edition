b016394361d099f62e1c60b9ba1c7b69b0b9bb01
AshleeGhil <ashleeghilardi@gmail.com>
2023-03-16 15:23:21 -0400

    Update StepDefinitions.java

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -33,22 +33,4 @@ public class StepDefinitions {
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
     }
-    
-    @Given("^Given I have dividend and divisor: (.*) and (.*)$")
-    public void i_have_dividend_and_divisor(String a, String b) throws Throwable {
-        this.a = a;
-        this.b = b;
-    }
-
-    @When("^When the calculator divides them$")
-    public void the_calculator_divides_them() throws Throwable {
-        String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
-        result = restTemplate.getForObject(url, String.class);
-    }
-
-    @Then("^I receive (.*) as the quotient$")
-    public void i_receive_as_a_result_div(String expectedResult) throws Throwable {
-        assertEquals(expectedResult, result);
-    }
-
 }
